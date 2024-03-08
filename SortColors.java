cae810cbce220c55a88707d2ac4f8a89beb32446
Sanath S <144911352+ssanath@users.noreply.github.com>
2024-03-08 17:00:40 +0530

    Create SortColors.java
    0-Red,1-Blue,2-Green

---
+++ SortColors.java
@@ -0,0 +1,32 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int zero=0,one=0,two=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==0)
+            {
+                zero+=1;
+            }
+            else if(nums[i]==1)
+            {
+                one+=1;
+            }
+            else
+            {
+                two+=1;
+            }
+        }
+         for(int i=0;i<zero;i++)
+    {
+        nums[i]=0;
+    }
+    for(int i=zero;i<(zero+one);i++)
+    {
+        nums[i]=1;
+    }
+    for(int i=(zero+one);i<(zero+one+two);i++)
+    {
+        nums[i]=2;
+    }
+    }
+}
