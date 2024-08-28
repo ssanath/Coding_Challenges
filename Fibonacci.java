b56f54f9971e0d7f5e92ce3a403e9dd74b7e5362
Sanath S <144911352+ssanath@users.noreply.github.com>
2024-08-28 20:28:40 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -10,11 +10,8 @@ class Solution {
         return fibonacci(n,dp);
     }
     public int fibonacci(int n, int[] dp) {
-        if(n==0) return 0;
-        if(n==1) return 1;
         if(dp[n] != -1) return dp[n];
-        dp[n-1] = fibonacci(n-1,dp);
-        dp[n-2] = fibonacci(n-2,dp);
-        return dp[n-1]+dp[n-2];
+        dp[n] = fibonacci(n-1,dp) + fibonacci(n-2,dp);
+        return dp[n];
     }
 }
