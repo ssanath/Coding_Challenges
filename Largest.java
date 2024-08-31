d31e8c5148cf3fd8a690a59e0aba7ff4b0b48014
Sanath S <144911352+ssanath@users.noreply.github.com>
2024-08-31 15:01:23 +0530

    Create Largest.java
    Given an array, arr. The task is to find the largest element in it.

---
+++ Largest.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static int largest(int[] arr) {
+        int largest = arr[0];
+        for(int i=1;i<arr.length;i++) {
+            if(arr[i]>largest) largest = arr[i];
+        }
+        return largest;
+    }
+}
